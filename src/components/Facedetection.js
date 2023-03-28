fae7758a049f2dd2e579e720c3463b4e2826ec0b
RahulPisharath <Rahul.Pisharath@ust-global.com>
2023-03-28 15:23:59 +0530

    Update the post method
    Update the  Code and post method

--- src/components/Facedetection.js
+++ src/components/Facedetection.js
@@ -12,6 +12,7 @@ const videoConstraints = {
 
 const Facedetection = () => {
   const [picture, setPicture] = useState("");
+  const [file, setfile] = useState("");
   const webcamRef = React.useRef(null);
 
   const capture = React.useCallback(() => {
@@ -20,9 +21,36 @@ const Facedetection = () => {
     console.log(pictureSrc);
   });
 
+  function handlechange(e) {
+    console.log(e.target.files);
+    setfile(URL.createObjectURL(e.target.files[0]));
+  }
+
+  async function compareImage(e) {
+    e.preventDefault();
+    // console.log("Image Comparison Button clicked!!!");
+
+    const formData = new FormData();
+    formData.append("file", setfile);
+
+    const data = await fetch("http://localhost:3000/upload/post", {
+      method: "post",
+      headers: { "Content-Type": "multipart/form-data" },
+      body: formData,
+    });
+
+    // const uploadedImage = await data.json();
+
+    if (data) {
+      console.log("Successfully uploaded image");
+    } else {
+      console.log("Error Found");
+    }
+  }
+
   return (
     <div className="face_ui">
-      <div className="face_area">
+      <div className="face_area capture_image grid_item">
         {picture == "" ? (
           <Webcam
             audio={false}
@@ -38,45 +66,57 @@ const Facedetection = () => {
       </div>
       <div>
         {picture !== "" ? (
-          <div className="butn_wrapper">
-            <button
-              onClick={() => {
-                window.location.reload();
-              }}
-              className="btn-primary d-flex align-items-center mx-10"
-            >
-              <span className="icon_scan">
-                <img className="w-100" alt="timer" src={FaceScan} />
-              </span>
-              Retake
-            </button>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                console.log("Compare FaceID");
-              }}
-              className="btn-primary d-flex align-items-center mx-10"
-            >
-              <span className="icon_scan">
-                <img className="w-100" alt="timer" src={compare} />
-              </span>
-              Compare
-            </button>
+          <div>
+            <div className="d-flex">
+              <div className="face_area grid_item">
+                <img src={picture} alt="User" />
+              </div>
+              <div className="face_area grid_item">
+                <div className="face_area grid_item">
+                  <img src={file} />
+                </div>
+                <input type="file" onChange={handlechange} />
+              </div>
+            </div>
+            <div className="butn_wrapper">
+              <button
+                onClick={() => {
+                  window.location.reload();
+                }}
+                className="btn-primary d-flex align-items-center mx-10"
+              >
+                <span className="icon_scan">
+                  <img className="w-100" alt="timer" src={FaceScan} />
+                </span>
+                Retake
+              </button>
+              <button
+                onClick={compareImage}
+                className="btn-primary d-flex align-items-center mx-10"
+              >
+                <span className="icon_scan">
+                  <img className="w-100" alt="timer" src={compare} />
+                </span>
+                Compare
+              </button>
+            </div>
           </div>
         ) : (
-          <div className="butn_wrapper">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                capture();
-              }}
-              className="btn-primary d-flex align-items-center"
-            >
-              <span className="icon_scan">
-                <img className="w-100" alt="timer" src={FaceScan} />
-              </span>
-              Capture
-            </button>
+          <div>
+            <div className="butn_wrapper">
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  capture();
+                }}
+                className="btn-primary d-flex align-items-center"
+              >
+                <span className="icon_scan">
+                  <img className="w-100" alt="timer" src={FaceScan} />
+                </span>
+                Capture
+              </button>
+            </div>
           </div>
         )}
       </div>
